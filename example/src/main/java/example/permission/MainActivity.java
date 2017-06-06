e4c2f09b6fb12da95dd04332311500a8474fc78c
hongyang <hongyooo@icloud.com>
2017-06-06 19:17:01 +0800

    version 1.1.4

--- example/src/main/java/example/permission/MainActivity.java
+++ example/src/main/java/example/permission/MainActivity.java
@@ -1,121 +1,30 @@
 package example.permission;
 
-import android.Manifest;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
-import java.util.Arrays;
-import java.util.List;
-
-import io.reactivex.Observer;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
-import rxkotlin.grace.permission.RxKotlinPermission;
-import rxkotlin.grace.permission.RxInteractive;
-import rxkotlin.grace.permission.RxRequest;
+import rxkotlin.grace.permission.KtPermission;
+import rxkotlin.grace.permission.launcher.LaunchTask;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    RxKotlinPermission rxKotlinPermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rxKotlinPermission = new RxKotlinPermission(this);
-        request();
-    }
 
-    /**
-     * hide Dialog
-     * default show
-     */
-    public  void hideInteractiveLayout(){
-        rxKotlinPermission.setCancelInteractive();
-    }
-
-    /**
-     *  Consumer impl
-     */
-    public  void request(){
-        rxKotlinPermission.requestCamera().subscribe(new Consumer<RxInteractive>() {
+        KtPermission permission = new KtPermission(this);
+        permission.requestCamera().launcher(new LaunchTask() {
             @Override
-            public void accept(@NonNull RxInteractive rxInteractive) throws Exception {
-                /**
-                 * success
-                 */
-                Log.e(TAG,"accept");
-            }
-        });
-    }
-
-    /**
-     * Observer impl
-     */
-    public  void requestCameraObserver(){
-        rxKotlinPermission.requestCamera().subscribe(new Observer<RxInteractive>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-
-            }
-
-            @Override
-            public void onNext(RxInteractive rxLimit) {
-                /**
-                 * success
-                 */
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onComplete() {
+            public void launch(boolean b) {
 
             }
         });
     }
 
-
-    /**
-     * requestCustom
-     */
-    public  void requestCustom(){
-        rxKotlinPermission.setCancelInteractive();
-        rxKotlinPermission.request(Manifest.permission.WRITE_SETTINGS,
-                Manifest.permission.WRITE_SETTINGS).subscribe(new Consumer<RxInteractive>() {
-            @Override
-            public void accept(@NonNull RxInteractive rxInteractive) throws Exception {
-                switch (rxInteractive.getRxMode()) {
-                    case GRACE_RECRY:
-                        Log.e(TAG, "GRACE_RECRY");
-                        break;
-                    case GRACE_ALLOW:
-                        Log.e(TAG, "GRACE_ALLOW");
-                        break;
-                    case GRACE_REFUSE:
-                        Log.e(TAG, "GRACE_REFUSE");
-                        break;
-                    case GRACE_HIDE:
-                        Log.e(TAG, "GRACE_HIDE");
-                        break;
-                }
-            }
-        });
-    }
-
-    /**
-     * customLayout
-     */
-   public void customLayout(){
-        rxKotlinPermission.settingRequest().setting(new RxRequest.Builder().title("Current permissions").message("Permission description").build());
-    }
 
 }
