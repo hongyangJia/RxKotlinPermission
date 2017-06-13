4d4fc479819d2cef0f0ea8972df64677de2adfbd
hongyang <hongyooo@icloud.com>
2017-06-13 10:13:01 +0800

    version 1.1.8

--- example/src/main/java/example/permission/Fragment2.java
+++ example/src/main/java/example/permission/Fragment2.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import rxkotlin.grace.permission.KtPermission;
+import rxkotlin.grace.permission.KtPermissionSetting;
 import rxkotlin.grace.permission.launcher.LaunchTask;
 
 /**
@@ -19,6 +20,8 @@ public class Fragment2 extends android.support.v4.app.Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        //KtPermissionSetting.INSTANCE.title("title").message("message").dialog(new DefaultTemplate()).build(this);
 
         KtPermission permission = new KtPermission(getActivity());
         permission.requestCameraMicroPhoneStorage().launcher(new LaunchTask() {
